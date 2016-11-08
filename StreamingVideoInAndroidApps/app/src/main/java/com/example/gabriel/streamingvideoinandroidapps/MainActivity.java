f888c61825cb952938e8340cd60093f63ce83d65
gabrielmirandat <gabrielmirandatt@gmail.com>
2016-11-08 12:08:50 -0200

    Added branch test for Streaming Video in Android Apps

---
+++ StreamingVideoInAndroidApps/app/src/main/java/com/example/gabriel/streamingvideoinandroidapps/MainActivity.java
@@ -0,0 +1,45 @@
+//https://code.tutsplus.com/tutorials/streaming-video-in-android-apps--cms-19888
+//libraries you can use to stream media files, such as remote videos
+//we will stream a video file
+//displaying it using the VideoView component together with a MediaController object to let the user control playback.
+//We will also briefly run through the process of presenting the video using the MediaPlayer class
+
+package com.example.gabriel.streamingvideoinandroidapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+
+        //String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+        //String vidAddress = "https://www.dropbox.com/s/uffooqf2buvrq1n/2016-09-23-145456.webm";
+        Uri vidUri = Uri.parse(vidAddress);
+        //We parse the address string as a URI so that we can pass it to the VideoView object:
+        vidView.setVideoURI(vidUri);
+
+        //We've implemented video playback, but the user will expect and be accustomed to having
+        //control over it. Again, the Android platform provides resources for handling this using familiar
+        //interaction via the MediaController class.
+        MediaController vidControl = new MediaController(this);
+
+        //Next, set it to use the VideoView instance as its anchor
+        vidControl.setAnchorView(vidView);
+
+        //And finally, set it as the media controller for the VideoView object:
+        vidView.setMediaController(vidControl);
+
+        //Now you can simply start playback
+        vidView.start();
+    }
+}
