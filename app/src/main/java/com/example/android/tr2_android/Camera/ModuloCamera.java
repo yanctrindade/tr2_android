f6534b4c67e699fc058af00d30788a4395f97bd7
tuliolima <tuliook@hotmail.com>
2016-11-24 10:45:00 -0200

    Criação do módulo da câmera
    Ele será usado para realizar as funções de tirar foto, gravar vídeo e
    fazer streaming.

---
+++ app/src/main/java/com/example/android/tr2_android/Camera/ModuloCamera.java
@@ -0,0 +1,160 @@
+package com.example.android.tr2_android.Camera;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Túlio on 22/11/2016.
+ */
+
+public class ModuloCamera {
+    private static final String TAG = "CamTestActivity";
+    Preview preview;
+    Button buttonClick;
+    Camera camera;
+    Context ctx;
+    File ultimoArquivo = null;
+
+    public ModuloCamera(Context context, Preview preview){
+        this.ctx = context;
+        this.preview = preview;
+        preview.setLayoutParams(new ViewGroup.LayoutParams(1, 1));
+
+        preview.setKeepScreenOn(true);
+
+        configurarCamera();
+    }
+
+    File tirarFoto(){
+        try {
+            ultimoArquivo.wait();
+            camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return  null;
+    }
+
+    protected void configurarCamera() {
+        int numCams = Camera.getNumberOfCameras();
+        if(numCams > 0){
+            try{
+                camera = Camera.open(0);
+                camera.startPreview();
+                preview.setCamera(camera);
+            } catch (RuntimeException ex){
+                Toast.makeText(ctx, "A camera nao foi encontrada", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    protected void onPause() {
+        if(camera != null) {
+            camera.stopPreview();
+            preview.setCamera(null);
+            camera.release();
+            camera = null;
+        }
+    }
+
+    private void resetCam() {
+        camera.startPreview();
+        preview.setCamera(camera);
+    }
+
+    private void refreshGallery(File file) {
+        Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(Uri.fromFile(file));
+        final Activity activity = (Activity) ctx;
+        activity.sendBroadcast(mediaScanIntent);
+    }
+
+    Camera.ShutterCallback shutterCallback = new Camera.ShutterCallback() {
+        public void onShutter() {
+            //			 Log.d(TAG, "onShutter'd");
+        }
+    };
+
+    Camera.PictureCallback rawCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            //			 Log.d(TAG, "onPictureTaken - raw");
+        }
+    };
+
+    Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            new SaveImageTask().execute(data);
+            resetCam();
+            Log.d(TAG, "onPictureTaken - jpeg");
+        }
+    };
+
+    private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+
+        @Override
+        protected Void doInBackground(byte[]... data) {
+            FileOutputStream outStream = null;
+
+            // Write to SD Card
+            try {
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File (sdCard.getAbsolutePath() + "/camtest");
+                dir.mkdirs();
+
+                String fileName = String.format("%d.jpg", System.currentTimeMillis());
+                File outFile = new File(dir, fileName);
+
+                outStream = new FileOutputStream(outFile);
+                outStream.write(data[0]);
+                outStream.flush();
+                outStream.close();
+
+                Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length + " to " + outFile.getAbsolutePath());
+
+                refreshGallery(outFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+            }
+            return null;
+        }
+
+    }
+
+    private class TimeoutOperation extends AsyncTask<String, Void, Void>{
+
+        @Override
+        protected Void doInBackground(String... params) {
+
+            try {
+                Log.i(TAG, "Going to sleep");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.i(TAG, "This is executed after 10 seconds and runs on the main thread");
+            super.onPostExecute(result);
+        }
+    }
+}
